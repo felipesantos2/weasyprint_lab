9d18f0d27bc0bec448654e8c11d63f2d9ae0ab84
felipesantos2 <santospinheiro627@gmail.com>
2026-08-17 13:47:50 -0300

    wip

--- app.py
+++ app.py
@@ -1,35 +1,37 @@
 """
-Executa o servidor para desenvolvimento
+Start Dev web server:
     flask run --reload
     uv run flask run --reload
+
+Run in terminal:
     uv run app.py
+    ruff format app.py
+    ruff check app.py
 """
 
-from flask import Flask
-from rich.console import Console
-from jinja2 import Environment, PackageLoader, select_autoescape, Template
-import tempfile
-from weasyprint import HTML
-from datetime import datetime
-from zoneinfo import ZoneInfo
 import os
+from datetime import datetime
 from pathlib import Path
+from zoneinfo import ZoneInfo
 
-
-RELATORIO_FINAL = "relatorio_anual_final.pdf"
-HTML_BASE_TEMPLATE = "template.jinja2.html"
-
-headers: dict[str, str] = {
-    "title": "Relatório Base",
-    "created_at ": str(datetime.now(tz=ZoneInfo("America/Sao_Paulo"))),
-}
-info: dict[str, str] = {"author": "@felipesantos2"}
+from flask import Flask  # noqa: F401
+from jinja2 import Template
+from rich.console import Console
+from weasyprint import HTML
 
 console = Console()
 
+HTML_BASE_TEMPLATE = "template.jinja2.html"
+
+settings: dict[str, str] = {
+    "title": "Relatório Base",
+    "author": "@felipesantos2",
+    "created_at ": str(datetime.now(tz=ZoneInfo("America/Sao_Paulo"))),
+}
+
 
 def load_html_template(file_path: str) -> str:
-    print("template base: ", file_path)
+    console.log("template base: ", file_path)
 
     with open(file_path, "r", encoding="utf-8") as f:
         template_str = f.read()
@@ -47,21 +49,25 @@ def write_html_file(html_out: str) -> None:
 
 
 def write_pdf_file(html_out: str) -> str:
+    import uuid
+
+    uuid4 = uuid.uuid4()
+
     Path("./templates/reports/pdf").mkdir(parents=True, exist_ok=True)
-    output_path = f"./templates/reports/pdf/{RELATORIO_FINAL}"
+    output_path = f"./templates/reports/pdf/{uuid4}.pdf"
 
     output_pdf = os.path.abspath(output_path)
     HTML(string=html_out).write_pdf(output_pdf)
     return output_pdf
 
 
-def main() -> None:
+def run() -> None:
     try:
         template_path = f"./templates/{HTML_BASE_TEMPLATE}"
         template_path = os.path.abspath(template_path)
         template_str = load_html_template(template_path)
 
-        merge_context_data = headers | info
+        merge_context_data: dict = settings
 
         template_objct = Template(template_str)
         html_out = render_jinja_template(template_objct, merge_context_data)
@@ -69,21 +75,21 @@ def main() -> None:
         write_html_file(html_out)
 
         tmp_pdf = write_pdf_file(html_out)
-    except Exception as e:
-        print(f"Erro! Exception: {e} ")
+    except Exception as e:  # noqa: BLE001
+        console.log(f"Erro! Exception: {e} ")
     else:
-        print("tmp file: ", tmp_pdf)
-        print("PDF Gerado com Sucesso! ")
+        console.log("tmp file: ", tmp_pdf)
+        console.log("PDF Gerado com Sucesso! ")
 
 
 # app = Flask(__name__)
 
-
 # @app.route("/")
 # def home():
+#     run()
 #     return "<h1>Hello, World!</h1>"
 
 
 if __name__ == "__main__":
-    main()
+    run()
     # app.run(host="127.0.0.1", port=8000, debug=True)
