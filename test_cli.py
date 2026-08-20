4af8e6e34e832b107198dd1b4928636923f8de35
felipesantos2 <santospinheiro627@gmail.com>
2026-08-19 22:26:48 -0300

    wip

--- test_cli.py
+++ test_cli.py
@@ -19,14 +19,13 @@ console = Console()
 
 PDF_DOC_OUTPUT = uuid.uuid4()
 
-HTML_BASE_TEMPLATE ="template.jinja2.html"
+HTML_BASE_TEMPLATE = Path("./templates/template.jinja2.html")
 
 
 # verifica se há um retorno de um html
 def test_jinja_html_template_loaded():
 	""" testa se o arquivo de template base é carregado """
-	template_path = Path(f"./templates/{HTML_BASE_TEMPLATE}")
-	template_str_content = load_html_template(template_path)
+	template_str_content = load_html_template(HTML_BASE_TEMPLATE)
 
 	assert "DOCTYPE" in template_str_content
 	assert "html" in template_str_content
@@ -34,8 +33,7 @@ def test_jinja_html_template_loaded():
 
 def test_jinja_html_template_generated():
 	""" testa se arquivo de template em html é gerado """
-	template_path = Path(f"./templates/{HTML_BASE_TEMPLATE}")
-	template_str_content = load_html_template(template_path)
+	template_str_content = load_html_template(HTML_BASE_TEMPLATE)
 
 	merge_context_data: dict = settings
 	html_out_content = render_jinja_template(Template(template_str_content), merge_context_data)
@@ -50,8 +48,7 @@ def test_jinja_html_template_generated():
 
 def test_pdf_document_is_generated():
 	""" testa se arquivo em pdf é gerado """
-	template_path = Path(f"./templates/{HTML_BASE_TEMPLATE}")
-	template_str_content = load_html_template(template_path)
+	template_str_content = load_html_template(HTML_BASE_TEMPLATE)
 
 	merge_context_data: dict = settings
 	html_out_content = render_jinja_template(Template(template_str_content), merge_context_data)
