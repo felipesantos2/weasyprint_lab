200c72c1b1823da3356b3e621c1d82026c00c87d
felipesantos2 <santospinheiro627@gmail.com>
2026-09-03 20:49:05 -0300

    wip

--- test_sample.py
+++ test_sample.py
@@ -1,16 +1,15 @@
 # pytest doc
 # https://docs.pytest.org/en/stable/index.html
+#
+# ARRANGE
+# SUT
+# 	não podemos ter um input e output verdadeiro, temos que mocar essas informações ou simular elas de alguma forma
+# ASSERT
 
 
 def func(x):
     return x + 1
 
 
-def test_answer() :
+def test_answer():
     assert func(4) == 5
-
-
-# ARRANGE
-# SUT
-# 	não podemos ter um input e output verdadeiro, temos que mocar essas informações ou simular elas de alguma forma
-# ASSERT
